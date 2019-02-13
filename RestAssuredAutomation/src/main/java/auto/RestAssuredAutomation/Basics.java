6e4fe5ba46208cb0c835cf06edd1c821dd330d4c
Ashutosh Chittora <AshutoshChittora@gmail.com>
2019-02-13 08:59:34 +0000

    hibernate changes

--- RestAssuredAutomation/src/main/java/auto/RestAssuredAutomation/Basics.java
+++ RestAssuredAutomation/src/main/java/auto/RestAssuredAutomation/Basics.java
@@ -12,7 +12,8 @@ import com.sun.org.apache.xpath.internal.operations.And;
 
 public class Basics { 
 
-	// BAseUri or host
+	// BAseUri or host - AIzaSyDaBwGzVkOHoTPIJphx2hirBhaiBFTjscw  (Place Search)
+	//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY
 
 	/*
 	 * given() - here need to provide Request - headers/cookies/parameters/body
